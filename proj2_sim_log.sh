2f2823938fb0b0041d8a8f14e07a878dba695612
yiichu03 <yiichu03@gmail.com>
2026-03-26 16:45:51 +0800

    flip imu gravity sign and add log runner

---
+++ proj2_sim_log.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o pipefail
+
+mkdir -p log/lab2
+timestamp=$(date +%Y%m%d_%H%M%S)
+log_file="log/lab2/proj2_sim_${timestamp}.log"
+
+echo "Writing proj2 log to ${log_file}"
+
+source install/setup.bash
+ros2 launch ee4308_bringup proj2_sim.launch.py libgl:=False 2>&1 | tee "${log_file}"
+./kill_gz.sh
